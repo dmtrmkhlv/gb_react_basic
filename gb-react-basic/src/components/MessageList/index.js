81341fcc3d62e8697b0e48366e5de1c5a372ffef
dmtrmkhlv <app@dmtrmkhlv.ru>
2022-02-05 19:48:20 +0300

    Перенос логики бота в middleware

--- gb-react-basic/src/components/MessageList/index.js
+++ gb-react-basic/src/components/MessageList/index.js
@@ -1,9 +1,9 @@
-import {useCallback, useEffect, useRef} from 'react';
+import {useCallback, useRef} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {Message} from "../";
 import styles from '../../styles/App.module.css';
 import {Button} from '@mui/material';
-import {addMessageAction} from "../../store/messages/actions";
+import {addMessageWithThunk} from "../../store/messages/actions";
 import {getMessagesFromReducer} from "../../store/messages/selectors";
 
 export const MessageList = (props) => {
@@ -14,31 +14,15 @@ export const MessageList = (props) => {
   const timeId = useRef(null);
   const inputFocus = useRef(null);
 
-  const addMessageWithThunk = (chatId, message, e) => (dispatch, getState) => {
-    dispatch(addMessageAction(chatId, message));
-    setTimeout(() => {e.target.parentNode.firstChild.firstChild.scrollIntoView({block: "end", behavior: "smooth"});
-     }, 0);
-    if (message.author !== "admin") {
-        if (timeId.current) {
-          clearTimeout(timeId.current);
-        }
-       const botMessage = "Hello! We have received your message and will reply soon.";
-       timeId.current = setTimeout(() => {
-         dispatch(addMessageAction(chatId, botMessage, "admin"));
-         e.target.parentNode.firstChild.firstChild.scrollIntoView({block: "end", behavior: "smooth"});
-        }, 1500);
-    }
-  }
-
   const pushNewMessage = useCallback((e) => {
     e.preventDefault();
     let message = e.target.elements.text.value;
     if (message.length > 0) {
       e.target.reset();
-      dispatch(addMessageWithThunk(chatId, message, e));
+      dispatch(addMessageWithThunk(chatId, message, e, timeId));
     }
     inputFocus.current.focus();
-  }, [dispatch]);
+  }, [chatId, dispatch]);
 
   return ( 
     <> 
--- gb-react-basic/src/store/messages/actions.js
+++ gb-react-basic/src/store/messages/actions.js
@@ -6,3 +6,19 @@ export const addMessageAction = (chatId, message, author) => ({
   message,
   author,
 });
+
+export const addMessageWithThunk = (chatId, message, e, timeId) => (dispatch, getState) => {
+  dispatch(addMessageAction(chatId, message));
+  setTimeout(() => {e.target.parentNode.firstChild.firstChild.scrollIntoView({block: "end", behavior: "smooth"});
+   }, 0);
+  if (message.author !== "admin") {
+      if (timeId.current) {
+        clearTimeout(timeId.current);
+      }
+     const botMessage = "Hello! We have received your message and will reply soon.";
+     timeId.current = setTimeout(() => {
+       dispatch(addMessageAction(chatId, botMessage, "admin"));
+       e.target.parentNode.firstChild.firstChild.scrollIntoView({block: "end", behavior: "smooth"});
+      }, 1500);
+  }
+}
